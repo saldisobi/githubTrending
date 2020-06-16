4044bef7762bdcec4159a19a762e0e24f30900d6
sourabh saldi <saldisaurabh99@gmail.com>
2020-06-16 09:43:42 +0530

    force test in view model

--- app/src/test/java/com/saldi/gittrending/ui/list/TrendingListViewModelTest.kt
+++ app/src/test/java/com/saldi/gittrending/ui/list/TrendingListViewModelTest.kt
@@ -5,7 +5,6 @@ import com.saldi.gittrending.data.model.ApiResponse
 import com.saldi.gittrending.data.model.BuiltBy
 import com.saldi.gittrending.data.model.TrendingListItem
 import com.saldi.gittrending.data.repository.TrendingRepository
-import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
@@ -14,7 +13,10 @@ import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
-import org.junit.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
 
@@ -55,8 +57,6 @@ class TrendingListViewModelTest {
     fun getPosts_setsTrendingData() =
         runBlockingTest {
             //given
-
-
             val builtByList = arrayListOf<BuiltBy>()
             builtByList.add(BuiltBy("", "", "saldi"))
 
@@ -72,6 +72,36 @@ class TrendingListViewModelTest {
 
             //when
             trendingListViewModel.getPosts("", "", "", false)
+
+            //then
+            assert(
+                trendingListViewModel?.trendingLiveData?.value?.data?.get(0)?.builtBy?.get(0)?.username == expectedResponse.data?.get(
+                    0
+                )?.builtBy?.get(0)?.username ?: ""
+            )
+        }
+
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun getForcePosts_setsTrendingData() =
+        runBlockingTest {
+            //given
+            val builtByList = arrayListOf<BuiltBy>()
+            builtByList.add(BuiltBy("", "", "saldi"))
+
+            val trendingListItem =
+                TrendingListItem(0, "", "", builtByList, 0, "", 0, "", "", "", 0, "")
+            val trendingList = arrayListOf<TrendingListItem>()
+            trendingList.add(trendingListItem)
+
+            val expectedResponse = ApiResponse.success(trendingList)
+
+            Mockito.`when`(trendingRepository.getTrending("", "", "", true))
+                .thenReturn(flowOf(expectedResponse as ApiResponse<*>) as Flow<ApiResponse<List<TrendingListItem>>>?)
+
+            //when
+            trendingListViewModel.getPosts("", "", "", true)
 
             //then
             assert(
